3e989e033f7ef189f4d10975c3b784ccfc7f9429
Kyle Barron <kylebarron2@gmail.com>
2018-01-02 17:50:40 -0500

    Add mac_install.sh

---
+++ mac_install.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+brew update
+brew install fzf
+brew install wget
+brew install hub
+brew install the_silver_searcher
+brew install tmux
+brew install pandoc
+brew install thefuck
+brew install lastpass-cli --with-pinentry --with-doc
